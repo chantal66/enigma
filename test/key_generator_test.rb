7fdbb5863cf36e24745544fc8cd351b0ebbd1d2e
Chantal Justamond <cjustamm66@me.com>
2017-03-24 12:17:04 -0600

    keys are not equal

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,4 +8,19 @@ class KeyGeneratorTest < Minitest::Test
 
     assert_instance_of KeyGenerator, key
   end
+  
+  def test_has_the_five_numbers_we_need
+    key = KeyGenerator.new
+    result = key.random_key_maker.length
+    
+    assert_equal 5 , result
+  end
+  def test_keys_are_not_the_same
+    key = KeyGenerator.new
+    key_1 = key.random_key_maker
+    key_2 = key.random_key_maker
+    result = (key_1 == key_2)
+    
+    refute false , result
+  end
 end
